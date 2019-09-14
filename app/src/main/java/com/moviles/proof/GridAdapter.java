6e352dca334dff51efc4f3208ddcc621501641ac
Andrés Osorio <gd_andres98@hotmail.com>
2019-09-13 21:35:25 -0500

    Foticos y más foticos :3

---
+++ app/src/main/java/com/moviles/proof/GridAdapter.java
@@ -0,0 +1,69 @@
+package com.moviles.proof;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import java.util.ArrayList;
+
+public class GridAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Bitmap> imageList;
+    private ArrayList<Uri> videoList;
+    private boolean isPictureGrid;
+
+    public GridAdapter(Context context, ArrayList<Bitmap> imageList, ArrayList<Uri> videoList) {
+
+        this.context = context;
+
+        if (videoList == null) {
+            this.imageList = imageList;
+            this.isPictureGrid = true;
+        } else {
+            this.imageList = imageList;
+            this.isPictureGrid = false;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return isPictureGrid ? imageList.size() : videoList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return isPictureGrid ? imageList.get(i) : videoList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if (view == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = isPictureGrid ? layoutInflater.inflate(R.layout.item_grid_image, null) : layoutInflater.inflate(R.layout.item_grid_image, null);
+        }
+
+        if (isPictureGrid) {
+            ImageView imageItem = (ImageView) view.findViewById(R.id.imageItem);
+            imageItem.setImageBitmap(imageList.get(i));
+        } else {
+            VideoView videoItem = (VideoView) view.findViewById(R.id.videoItem);
+            videoItem.setVideoURI(videoList.get(i));
+        }
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/moviles/proof/MainActivity.java
+++ app/src/main/java/com/moviles/proof/MainActivity.java
@@ -4,25 +4,48 @@ package com.moviles.proof;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**Buttons*/
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_VIDEO_CAPTURE = 2;
+
+    /**
+     * Buttons
+     */
     private ImageButton cameraId;
     private ImageButton videoId;
     private ImageButton galleryId;
 
-    /**Layouts*/
+    /**
+     * Layouts
+     */
     private TextView lcameraId;
     private TextView lvideoId;
     private TextView lgalleryId;
+
+    private GridView imageGridViewId;
+    private GridAdapter imageAdapter;
+    private GridAdapter videoAdapter;
+
+    private ArrayList<Bitmap> images;
+    private ArrayList<Uri> videos;
 
 
     @Override
@@ -30,30 +53,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraId = findViewById(R.id.cameraId);
-        videoId = findViewById(R.id.videoId);
-        galleryId = findViewById(R.id.galleryId);
+        images = new ArrayList<>();
+        videos = new ArrayList<>();
 
-        lcameraId = findViewById(R.id.lcameraId);
-        lvideoId = findViewById(R.id.lvideoId);
-        lgalleryId = findViewById(R.id.lgalleryId);
+        lcameraId = (TextView) findViewById(R.id.lcameraId);
+        lvideoId = (TextView) findViewById(R.id.lvideoId);
+        lgalleryId = (TextView) findViewById(R.id.lgalleryId);
+
+        imageGridViewId = (GridView) findViewById(R.id.captureGridView);
+        imageAdapter = new GridAdapter(this,images,null);
+        imageGridViewId.setAdapter(imageAdapter);
+        videoAdapter = new GridAdapter(this,null,videos);
+
+
     }
 
-    public void galleryButton(View view){
+    public void galleryButton(View view) {
         lgalleryId.setTextColor(Color.parseColor("#2E51F0"));
         lcameraId.setTextColor(Color.parseColor("#B8B9BD"));
         lvideoId.setTextColor(Color.parseColor("#B8B9BD"));
     }
 
-    public void cameraButton(View view){
+    public void cameraButton(View view) {
         lcameraId.setTextColor(Color.parseColor("#2E51F0"));
         lvideoId.setTextColor(Color.parseColor("#B8B9BD"));
         lgalleryId.setTextColor(Color.parseColor("#B8B9BD"));
     }
-    public void videoButton(View view){
+
+    public void videoButton(View view) {
         lvideoId.setTextColor(Color.parseColor("#2E51F0"));
         lgalleryId.setTextColor(Color.parseColor("#B8B9BD"));
         lcameraId.setTextColor(Color.parseColor("#B8B9BD"));
     }
+
+
+    public void takePic(View view) {
+        Toast.makeText(this, "Tomar Foto", Toast.LENGTH_SHORT).show();
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    public void takeVid(View view) {
+        Toast.makeText(this, "Tomar Video", Toast.LENGTH_SHORT).show();
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Toast.makeText(this, "Tome su foto :v", Toast.LENGTH_SHORT).show();
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            images.add(imageBitmap);
+        }
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            videos.add(data.getData());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 
 }
